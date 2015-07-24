f399c64328891b5464a9986b919f738f8bb1c330
App Academy Student <student@appacademy.io>
2015-07-23 18:00:55 -0700

    implement core reversi

--- js-reversi-master/skeleton/lib/board.js
+++ js-reversi-master/skeleton/lib/board.js
@@ -5,6 +5,20 @@ var Piece = require("./piece");
  * and two white pieces at [3, 3] and [4, 4]
  */
 function _makeGrid () {
+  var grid = [];
+  for (var i = 0; i < 8; i++) {
+    row = [];
+    for (var j = 0; j < 8; j++) {
+      row.push(' ');
+    }
+    grid.push(row);
+  }
+  grid[3][4] = new Piece ("black");
+  grid[4][3] = new Piece ("black");
+  grid[3][3] = new Piece ("white");
+  grid[4][4] = new Piece ("white");
+
+  return grid;
 }
 
 /**
@@ -15,9 +29,9 @@ function Board () {
 }
 
 Board.DIRS = [
-  [ 0,  1], [ 1,  1], [ 1,  0],
-  [ 1, -1], [ 0, -1], [-1, -1],
-  [-1,  0], [-1,  1]
+  [-1, -1], [-1,  0], [-1,  1],
+  [ 0, -1],           [ 0,  1],
+  [ 1, -1], [ 1,  0], [ 1,  1]
 ];
 
 /**
@@ -25,12 +39,16 @@ Board.DIRS = [
  * throwing an Error if the position is invalid.
  */
 Board.prototype.getPiece = function (pos) {
+  var x = pos[0]
+  var y = pos[1]
+  return this.grid[x][y]
 };
 
 /**
  * Checks if there are any valid moves for the given color.
  */
 Board.prototype.hasMove = function (color) {
+  return ( this.validMoves.length > 0 );
 };
 
 /**
@@ -38,12 +56,15 @@ Board.prototype.hasMove = function (color) {
  * matches a given color.
  */
 Board.prototype.isMine = function (pos, color) {
+  var item = this.getPiece(pos)
+  return (item instanceof Piece && item.color === color.toLowerCase() )
 };
 
 /**
  * Checks if a given position has a piece on it.
  */
 Board.prototype.isOccupied = function (pos) {
+  return (this.getPiece(pos) instanceof Piece)
 };
 
 /**
@@ -51,12 +72,16 @@ Board.prototype.isOccupied = function (pos) {
  * the black player are out of moves.
  */
 Board.prototype.isOver = function () {
+  return ( !( this.hasMoves("white") && this.hasMoves("black") ) );
 };
 
 /**
  * Checks if a given position is on the Board.
  */
 Board.prototype.isValidPos = function (pos) {
+  var x = pos[0];
+  var y = pos[1];
+  return ( x >= 0 && x < 8 && y >=0 && y < 8 );
 };
 
 /**
@@ -73,6 +98,26 @@ Board.prototype.isValidPos = function (pos) {
  * Returns null if no pieces of the opposite color are found.
  */
 function _positionsToFlip (board, pos, color, dir, piecesToFlip) {
+  var x_new = pos[0] + dir[0];
+  var y_new = pos[1] + dir[1];
+  var new_pos = [x_new, y_new]
+
+  if ( !board.isValidPos (pos) ) { // off the board, can't flip
+    return null;
+
+  } else if (! board.isOccupied( new_pos )) { //empty spaces can't help flip
+    return null;
+
+  } else if (! board.isMine( new_pos , color) ) { //add this opponent piece
+    piecesToFlip.push (board.getPiece(new_pos) );
+    return (_positionsToFlip (board, new_pos, color, dir, piecesToFlip) );
+
+  } else if (piecesToFlip.length > 0) {
+    return piecesToFlip; //hits my own piece and there are opponent pieces to flip
+
+  } else {
+    return null; //my own piece only. null.
+  }
 }
 
 /**
@@ -88,6 +133,10 @@ Board.prototype.placePiece = function (pos, color) {
  * Prints a string representation of the Board to the console.
  */
 Board.prototype.print = function () {
+  for (var i = 0; i < this.grid.length; i++) {
+    return
+    console.log ( this.grid[i].join("") );
+  };
 };
 
 /**
@@ -96,6 +145,20 @@ Board.prototype.print = function () {
  * color being flipped.
  */
 Board.prototype.validMove = function (pos, color) {
+  if (! self.isValidPos(pos) ) {
+    throw new Error "pos not on board";
+  } else if ( self.isOccupied(pos) ) {
+    return false;
+  } else {
+    for (var i = 0; i < 8; i++) {
+      var dir = Board.DIRS[i]
+      if ( _positionsToFlip(this, pos, color, dir, []).length > 0 ) {
+        return true; //if there are
+      }
+    }
+
+    return false;
+  }
 };
 
 /**
@@ -103,6 +166,14 @@ Board.prototype.validMove = function (pos, color) {
  * the Board for a given color.
  */
 Board.prototype.validMoves = function (color) {
+  var valids = []
+  for (var x = 0; x < 8; x++) {
+    for (var y = 0; y < 8; y++) {
+      if ( this.validMove([[x],[y]])) { valids.push([x, y]) }
+    }
+  }
+
+  return valids
 };
 
 module.exports = Board;
--- js-reversi-master/skeleton/lib/piece.js
+++ js-reversi-master/skeleton/lib/piece.js
@@ -2,25 +2,39 @@
  * Initializes the Piece with its color.
  */
 function Piece (color) {
+  if (color.toLowerCase() === "white") {
+    this.color = "white"
+  } else {
+    this.color = "black"
+  }
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+  if (this.color === "white") {
+    return "black";
+  } else {
+    return "white";
+  }
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+  if (this.color === "white") {
+    this.color = "black";
+  } else {
+    this.color = "white";
+  }
 };
 
 /**
  * Returns a string representation of the string
  * based on its color.
  */
-Piece.prototype.toString = function () {
-};
+
 
 module.exports = Piece;
